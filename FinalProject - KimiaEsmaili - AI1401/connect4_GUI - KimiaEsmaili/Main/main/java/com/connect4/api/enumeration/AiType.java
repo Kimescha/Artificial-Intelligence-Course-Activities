a1beb52bc452894091e05c4912c378b90424a193
Kimia Es <74457278+Kimescha@users.noreply.github.com>
2023-09-20 12:24:21 +0330

    Delete FinalProject - KimiaEsmaili - AI1401 directory

--- FinalProject - KimiaEsmaili - AI1401/connect4_GUI - KimiaEsmaili/Main/main/java/com/connect4/api/enumeration/AiType.java
+++
@@ -1,6 +0,0 @@
-package com.chriskormaris.connect4.api.enumeration;
-
-public enum AiType {
-	MINIMAX_AI,
-	RANDOM_AI
-}
--- FinalProject - KimiaEsmaili - AI1401/connect4_GUI - KimiaEsmaili/Main/main/java/com/connect4/gui/enumeration/GuiStyle.java
+++
@@ -1,7 +0,0 @@
-package com.chriskormaris.connect4.gui.enumeration;
-
-public enum GuiStyle {
-	SYSTEM_STYLE,
-	CROSS_PLATFORM_STYLE,
-	NIMBUS_STYLE
-}
